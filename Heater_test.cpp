6abf58e9505216cd1348ccf176586e1af26566bd
Udo Herrmann <herrmann.udo@gmx.de>
2017-05-28 17:14:06 +0200

    refactoring

--- Heater_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Heater_test.cpp
- *
- *  Created on: 28.05.2017
- *      Author: Udo
- */
-
-#include "Heater.h"
-
--- Pump_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Pump_test.cpp
- *
- *  Created on: 28.05.2017
- *      Author: Udo
- */
-
-#include "Pump.h"
-
